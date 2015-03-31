df61c29a0a9e3b5f63a13d9aba3647bac21623a2
Vinayak Menon <vinmenon@codeaurora.org>
2015-03-31 11:06:29 +0530

    mm: vmpressure: scale pressure based on reclaim context
    The existing calculation of vmpressure takes into account only
    the ratio of reclaimed to scanned pages, but not the time spent
    or the difficulty in reclaiming those pages. For e.g. when there
    are quite a number of file pages in the system, an allocation
    request can be satisfied by reclaiming the file pages alone. If
    such a reclaim is successful, the vmpressure value will remain low
    irrespective of the time spent by the reclaim code to free up the
    file pages. With a feature like lowmemorykiller, killing a task
    can be faster than reclaiming the file pages alone. So if the
    vmpressure values reflect the reclaim difficulty level, clients
    can make a decision based on that, for e.g. to kill a task early.
    
    This patch monitors the number of pages scanned in the direct
    reclaim path and scales the vmpressure level according to that.
    
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Change-Id: I6e643d29a9a1aa0814309253a8b690ad86ec0b13

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -56,11 +56,14 @@ static unsigned long vmpressure_scale_max = 100;
 module_param_named(vmpressure_scale_max, vmpressure_scale_max,
 			ulong, S_IRUGO | S_IWUSR);
 
+<<<<<<< HEAD
 /* vmpressure values >= this will be scaled based on allocstalls */
 static unsigned long allocstall_threshold = 70;
 module_param_named(allocstall_threshold, allocstall_threshold,
 			ulong, S_IRUGO | S_IWUSR);
 
+=======
+>>>>>>> 29e2aa448fca... mm: vmpressure: scale pressure based on reclaim context
 static struct vmpressure global_vmpressure;
 BLOCKING_NOTIFIER_HEAD(vmpressure_notifier);
 
@@ -178,12 +181,17 @@ out:
 static unsigned long vmpressure_account_stall(unsigned long pressure,
 				unsigned long stall, unsigned long scanned)
 {
+<<<<<<< HEAD
 	unsigned long scale;
 
 	if (pressure < allocstall_threshold)
 		return pressure;
 
 	scale = ((vmpressure_scale_max - pressure) * stall) / scanned;
+=======
+	unsigned long scale =
+		((vmpressure_scale_max - pressure) * stall) / scanned;
+>>>>>>> 29e2aa448fca... mm: vmpressure: scale pressure based on reclaim context
 
 	return pressure + scale;
 }
