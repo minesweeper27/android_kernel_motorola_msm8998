c489bdebad71e907efe538ad8badaddda121aea7
seba3567 <sebastian26481@gmail.com>
2019-06-14 12:42:59 -0400

    beta 5.2 is present

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -573,6 +573,10 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive_pro)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
