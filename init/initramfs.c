2a6c293502ee19c64aa0314e530ac75431e690fb
seba3567 <sebastian26481@gmail.com>
2019-06-18 14:39:54 -0400

    ramdisk bypass

--- init/initramfs.c
+++ init/initramfs.c
@@ -621,11 +621,7 @@ static int __init populate_rootfs(void)
 {
 	char *err;
 
-	if (do_skip_initramfs) {
-		if (initrd_start)
-			free_initrd();
-		return default_rootfs();
-	}
+	bool skip_ramdisk;
 
 	err = unpack_to_rootfs(__initramfs_start, __initramfs_size);
 	if (err)
@@ -638,6 +634,7 @@ static int __init populate_rootfs(void)
 			initrd_end - initrd_start);
 		if (!err) {
 			free_initrd();
+			skip_ramdisk = true;
 			goto done;
 		} else {
 			clean_rootfs();
@@ -663,8 +660,11 @@ static int __init populate_rootfs(void)
 		printk(KERN_INFO "Unpacking initramfs...\n");
 		err = unpack_to_rootfs((char *)initrd_start,
 			initrd_end - initrd_start);
-		if (err)
+		if (err) {
 			printk(KERN_EMERG "Initramfs unpacking failed: %s\n", err);
+			skip_ramdisk = true;
+		}
+
 		free_initrd();
 #endif
 		/*
@@ -673,6 +673,6 @@ static int __init populate_rootfs(void)
 		 */
 		load_default_modules();
 	}
-	return 0;
+	return skip_ramdisk ? default_rootfs() : 0;
 }
 rootfs_initcall(populate_rootfs);
