1307bf42c4a722f109163283e7ab53096e14b0d0
seba3567 <sebastian26481@gmail.com>
2019-12-06 20:59:36 -0300

    low latency patch

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -227,7 +227,7 @@ static int fpc1020_probe(struct platform_device *pdev)
 	wake_lock_init(&fpc1020->wlock, WAKE_LOCK_SUSPEND, "fpc1020");
 
 	fpc1020->irq_cnt = 0;
-	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
+	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL;;
 
 	rc = devm_request_threaded_irq(dev, gpio_to_irq(fpc1020->irq_gpio),
 			NULL, fpc1020_irq_handler, irqf,
