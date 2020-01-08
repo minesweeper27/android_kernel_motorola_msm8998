edea4e69c0e5d30e5b18ebb74ea93b318426e30c
seba_3567 <sebastian26481@gmail.com>
2020-01-08 01:35:51 -0300

    fix fair builders

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3617,31 +3617,6 @@ void _inc_hmp_sched_stats_fair(struct rq *rq,
 
 #endif	/* CONFIG_CFS_BANDWIDTH */
 
-<<<<<<< HEAD
-u32 sched_get_wake_up_idle(struct task_struct *p)
-{
-	return 0;
-}
-EXPORT_SYMBOL(sched_get_wake_up_idle);
-
-int sched_set_wake_up_idle(struct task_struct *p, int wake_up_idle)
-{
-	return 0;
-}
-EXPORT_SYMBOL(sched_set_wake_up_idle);
-
-#ifdef CONFIG_SMP
-static const u32 runnable_avg_yN_inv[] = {
-	0xffffffff, 0xfa83b2da, 0xf5257d14, 0xefe4b99a, 0xeac0c6e6, 0xe5b906e6,
-	0xe0ccdeeb, 0xdbfbb796, 0xd744fcc9, 0xd2a81d91, 0xce248c14, 0xc9b9bd85,
-	0xc5672a10, 0xc12c4cc9, 0xbd08a39e, 0xb8fbaf46, 0xb504f333, 0xb123f581,
-	0xad583ee9, 0xa9a15ab4, 0xa5fed6a9, 0xa2704302, 0x9ef5325f, 0x9b8d39b9,
-	0x9837f050, 0x94f4efa8, 0x91c3d373, 0x8ea4398a, 0x8b95c1e3, 0x88980e80,
-	0x85aac367, 0x82cd8698,
-};
-
-=======
->>>>>>> parent of 4505bd1ecd77... sched: Add stub functions for wake_up_idle API
 /*
  * Reset balance_interval at all sched_domain levels of given cpu, so that it
  * honors kick.
