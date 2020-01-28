e31b13e666dae18ff555092c8a63920a4c52250e
seba_3567 <sebastian26481@gmail.com>
2020-01-27 23:19:56 -0300

    Revert "treewide: Remove tracing and debug drivers"
    This reverts commit 3e8ffb79765bf15b00ed2b4246ed8b47eed37b32.

--- drivers/media/platform/msm/vidc/msm_vidc_debug.h
+++ drivers/media/platform/msm/vidc/msm_vidc_debug.h
@@ -123,20 +123,7 @@ struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 void msm_vidc_debugfs_deinit_inst(struct msm_vidc_inst *inst);
 void msm_vidc_debugfs_update(struct msm_vidc_inst *inst,
 		enum msm_vidc_debugfs_event e);
-<<<<<<< HEAD
 
-=======
-#else
-static inline struct dentry *msm_vidc_debugfs_init_drv(void) { return NULL; }
-static inline struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
-		struct dentry *parent) { return NULL; }
-static inline struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
-		struct dentry *parent) { return NULL; }
-static inline void msm_vidc_debugfs_deinit_inst(struct msm_vidc_inst *inst) {}
-static inline void msm_vidc_debugfs_update(struct msm_vidc_inst *inst,
-		enum msm_vidc_debugfs_event e) {}
-#endif
->>>>>>> b913c2c1f312... treewide: Remove tracing and debug drivers
 static inline void tic(struct msm_vidc_inst *i, enum profiling_points p,
 				 char *b)
 {
