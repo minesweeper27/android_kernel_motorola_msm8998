f217aaf8a3edd0a85750070c1fe3e2b54afffbee
seba_3567 <sebastian26481@gmail.com>
2020-02-14 15:38:33 -0300

    Revert "arm64: use simpler API for random address requests"
    This reverts commit a68ae281ce461868a6b6a752100db6f0335425ba.

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -485,7 +485,6 @@ unsigned long arch_align_stack(unsigned long sp)
 
 static unsigned long randomize_base(unsigned long base)
 {
-<<<<<<< HEAD
 	unsigned long range_end = base + (STACK_RND_MASK << PAGE_SHIFT) + 1;
 	return randomize_range(base, range_end, 0) ? : base;
 }
@@ -493,10 +492,4 @@ static unsigned long randomize_base(unsigned long base)
 unsigned long arch_randomize_brk(struct mm_struct *mm)
 {
 	return randomize_base(mm->brk);
-=======
-	if (is_compat_task())
-		return randomize_page(mm->brk, 0x02000000);
-	else
-		return randomize_page(mm->brk, 0x40000000);
->>>>>>> 735c4b4795cc... arm64: use simpler API for random address requests
 }
