936a61d16e9bb20d990c3008ea9857fb4a1a6a83
meg4z0rd <meydiwahendra@gmail.com>
2020-02-17 13:52:11 +0700

    Merge tag 'v4.4.214' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into unified
    This is the 4.4.214 stable release

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -7597,8 +7597,13 @@ static int ufshcd_probe_hba(struct ufs_hba *hba)
 			ufshcd_init_icc_levels(hba);
 
 		/* Add required well known logical units to scsi mid layer */
+<<<<<<< HEAD
 		if (ufshcd_scsi_add_wlus(hba)) {
 			pr_err("%s: ufshcd_scsi_add_wlus failed\n", __func__);
+=======
+		ret = ufshcd_scsi_add_wlus(hba);
+		if (ret)
+>>>>>>> 2cd93a390fff... Merge tag 'v4.4.214' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into unified
 			goto out;
 		}
 
