03fa40534840c9c01236c38f7a8bbfa4eddfcbbc
seba_3567 <sebastian26481@gmail.com>
2020-03-02 17:14:44 -0300

    Revert "lmk update"
    This reverts commit 7d7df2f3d5be81b287479993faf24c3e785d79fe.

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -10,7 +10,6 @@
 #include <linux/moduleparam.h>
 #include <linux/oom.h>
 #include <linux/sort.h>
-#include <linux/vmpressure.h>
 
 /* The minimum number of pages to free per reclaim */
 #define MIN_FREE_PAGES (CONFIG_ANDROID_SIMPLE_LMK_MINFREE * SZ_1M / PAGE_SIZE)
@@ -73,6 +72,7 @@ static bool vtsk_is_duplicate(int vlen, struct task_struct *vtsk)
 	return false;
 }
 
+<<<<<<< HEAD
 static unsigned long get_total_mm_pages(struct mm_struct *mm)
 {
 	unsigned long pages = 0;
@@ -86,6 +86,9 @@ static unsigned long get_total_mm_pages(struct mm_struct *mm)
 
 static unsigned long find_victims(int *vindex, short target_adj_min,
 				  short target_adj_max)
+=======
+static unsigned long find_victims(int *vindex, short target_adj)
+>>>>>>> parent of 7d7df2f3d5be... lmk update
 {
 	unsigned long pages_found = 0;
 	int old_vindex = *vindex;
@@ -120,7 +123,7 @@ static unsigned long find_victims(int *vindex, short target_adj_min,
 		/* Store this potential victim away for later */
 		victims[*vindex].tsk = vtsk;
 		victims[*vindex].mm = vtsk->mm;
-		victims[*vindex].size = get_total_mm_pages(vtsk->mm);
+		victims[*vindex].size = get_mm_rss(vtsk->mm);
 
 		/* Keep track of the number of pages that have been found */
 		pages_found += victims[*vindex].size;
@@ -157,10 +160,11 @@ static int process_victims(int vlen, unsigned long pages_needed)
 		/* The victim's mm lock is taken in find_victims; release it */
 		if (pages_found >= pages_needed) {
 			task_unlock(vtsk);
-		} else {
-			pages_found += victim->size;
-			nr_to_kill++;
+			continue;
 		}
+
+		pages_found += victim->size;
+		nr_to_kill++;
 	}
 
 	return nr_to_kill;
@@ -260,12 +264,39 @@ static int simple_lmk_reclaim_thread(void *data)
 	sched_setscheduler_nocheck(current, SCHED_FIFO, &sched_max_rt_prio);
 
 	while (1) {
-		wait_event(oom_waitq, atomic_read(&needs_reclaim));
+<<<<<<< HEAD
+		wait_event(oom_waitq, atomic_read_acquire(&needs_reclaim));
 		scan_and_kill(MIN_FREE_PAGES);
 		atomic_set_release(&needs_reclaim, 0);
+=======
+		wait_event(oom_waitq, atomic_add_unless(&needs_reclaim, -1, 0));
+		scan_and_kill(MIN_FREE_PAGES);
+>>>>>>> 77c531f66919... simple_lmk: Make reclaim deterministic
 	}
 
 	return 0;
+}
+
+void simple_lmk_decide_reclaim(int kswapd_priority)
+{
+<<<<<<< HEAD
+	if (kswapd_priority == CONFIG_ANDROID_SIMPLE_LMK_AGGRESSION &&
+	    !atomic_cmpxchg(&needs_reclaim, 0, 1))
+		wake_up(&oom_waitq);
+=======
+	if (kswapd_priority == CONFIG_ANDROID_SIMPLE_LMK_AGGRESSION) {
+		int v, v1;
+
+		for (v = 0;; v = v1) {
+			v1 = atomic_cmpxchg(&needs_reclaim, v, v + 1);
+			if (likely(v1 == v)) {
+				if (!v)
+					wake_up(&oom_waitq);
+				break;
+			}
+		}
+	}
+>>>>>>> 77c531f66919... simple_lmk: Make reclaim deterministic
 }
 
 void simple_lmk_mm_freed(struct mm_struct *mm)
@@ -274,30 +305,14 @@ void simple_lmk_mm_freed(struct mm_struct *mm)
 
 	read_lock(&mm_free_lock);
 	for (i = 0; i < victims_to_kill; i++) {
-		if (victims[i].mm != mm)
-			continue;
-
-		victims[i].mm = NULL;
-		if (atomic_inc_return_relaxed(&nr_killed) == victims_to_kill)
-			complete(&reclaim_done);
-		break;
+		if (cmpxchg(&victims[i].mm, mm, NULL) == mm) {
+			if (atomic_inc_return(&nr_killed) == victims_to_kill)
+				complete(&reclaim_done);
+			break;
+		}
 	}
 	read_unlock(&mm_free_lock);
 }
-
-static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
-				    unsigned long pressure, void *data)
-{
-	if (pressure == 100 && !atomic_cmpxchg_acquire(&needs_reclaim, 0, 1))
-		wake_up(&oom_waitq);
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block vmpressure_notif = {
-	.notifier_call = simple_lmk_vmpressure_cb,
-	.priority = INT_MAX
-};
 
 /* Initialize Simple LMK when lmkd in Android writes to the minfree parameter */
 static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
@@ -309,7 +324,6 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 		thread = kthread_run_perf_critical(simple_lmk_reclaim_thread,
 						   NULL, "simple_lmkd");
 		BUG_ON(IS_ERR(thread));
-		BUG_ON(vmpressure_notifier_register(&vmpressure_notif));
 	}
 
 	return 0;
