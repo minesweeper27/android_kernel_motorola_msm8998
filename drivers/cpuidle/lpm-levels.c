a9ee05bf636b9d8c1886df632323f17d3d76194f
seba_3567 <sebastian26481@gmail.com>
2020-03-05 19:27:04 -0300

    Revert "TREEWIDE: fix all clang 10 warning"
    This reverts commit 721cb4f7dd9cc8ec37ad01815e35590950671d91.

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1802,6 +1802,7 @@ static int lpm_probe(struct platform_device *pdev)
 {
 	int ret;
 	struct kobject *module_kobj = NULL;
+	struct md_region md_entry;
 
 	get_online_cpus();
 	lpm_root_node = lpm_of_parse_cluster(pdev);
